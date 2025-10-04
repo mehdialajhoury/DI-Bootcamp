233d077eeb1c3478e276f19dd882774acca630bf
mehdialajhoury <m_alajhoury@stu-pstb.fr>
2025-10-04 09:47:30 +0200

    add ExerciseXP Ninja

---
+++ Week1/Day1/ExerciseXP/ninja.py
@@ -0,0 +1,46 @@
+# #Predict the output of the following code snippets:
+
+# 3 <= 3 < 9
+
+# 3 == 3 == 3
+
+# bool(0)
+
+# bool(5 == "5")
+
+# bool(4 == 4) == bool("4" == "4")
+
+# bool(bool(None))
+
+# x = (1 == True)
+# y = (1 == False)
+# a = True + 4
+# b = False + 10
+
+#Without print() function, the program prints nothing for the first instructions
+
+# print("x is", x)
+# print("y is", y)
+# print("a:", a)
+# print("b:", b)
+
+# x is True
+# y is False
+# a: 5
+# b: 10
+
+# Keep asking the user to input the longest sentence they can without the character “A”.
+# Each time a user successfully sets a new longest sentence, print a congratulations message.
+
+flag = True
+
+while flag:
+    longest_sentence = input("Write the longest sentence without the lettre 'A'")
+
+    for letter in longest_sentence:
+        if letter == "A":
+            flag = False
+    sentence_heigth = len(longest_sentence)
+    if flag is True:
+        print("Try again ! There is no 'A' in your sentence !")
+print(f"Congratulations ! Your sentence contains {sentence_heigth} letters with the letter 'a")
