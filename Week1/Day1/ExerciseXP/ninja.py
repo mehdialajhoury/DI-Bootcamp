c6372c0c25a31299f69b6bbee8b745e121359792
mehdialajhoury <m_alajhoury@stu-pstb.fr>
2025-10-04 10:01:07 +0200

    correction  ExerciseXP Ninja

--- Week1/Day1/ExerciseXP/ninja.py
+++ Week1/Day1/ExerciseXP/ninja.py
@@ -32,15 +32,19 @@
 # Keep asking the user to input the longest sentence they can without the character “A”.
 # Each time a user successfully sets a new longest sentence, print a congratulations message.
 
+#initialization of the flag at True. While it is True, the game runs.
 flag = True
 
 while flag:
     longest_sentence = input("Write the longest sentence without the lettre 'A'")
 
+    # loop for comparing each letter in the longest sentence, if a "A" is detected, we set the flag at False,
+    # Then the game stops and a message is displayed to say that a "A" is detected
+    # Else, the game continues with a message counting the number of letters without "A"
     for letter in longest_sentence:
         if letter == "A":
             flag = False
     sentence_heigth = len(longest_sentence)
     if flag is True:
-        print("Try again ! There is no 'A' in your sentence !")
-print(f"Congratulations ! Your sentence contains {sentence_heigth} letters with the letter 'a")
+        print(f"Congratulations ! Your sentence contains {sentence_heigth} letters with the letter 'A'")
+print("Try again ! There is a A in your sentence")
