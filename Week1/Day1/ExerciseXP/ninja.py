8ed3197a5476d21ccf99473812db96b8873d4147
mehdialajhoury <m_alajhoury@stu-pstb.fr>
2025-10-04 17:26:11 +0200

    End of  ExerciseXP Ninja

--- Week1/Day1/ExerciseXP/ninja.py
+++ Week1/Day1/ExerciseXP/ninja.py
@@ -48,3 +48,37 @@ while flag:
     if flag is True:
         print(f"Congratulations ! Your sentence contains {sentence_heigth} letters with the letter 'A'")
 print("Try again ! There is a A in your sentence")
+
+# Find an interesting paragraph of text online. (Please keep it appropriate to the social context of our class.)
+# Paste it to your code, and store it in a variable.
+# Let’s analyze the paragraph. Print out a nicely formatted message saying:
+# How many characters it contains (this one is easy…).
+# How many sentences it contains.
+# How many words it contains.
+# How many unique words it contains.
+# Bonus: How many non-whitespace characters it contains.
+# Bonus: The average amount of words per sentence in the paragraph.
+# Bonus: the amount of non-unique words in the paragraph.
+
+sentence = "It is a sentence. It is nice."
+sentence_length = len(sentence)
+
+
+list_sentence_split = sentence.split()
+number_words = len(list_sentence_split)
+print(f"The sentence length is {sentence_length}")
+print(f"There are {sentence.count(".")} sentences")
+print(f"There are {number_words} words")
+
+list_sentence_split = sentence.split()
+
+# For searching unique words, we use a void list in which we add a word if it does not exists
+# By looping through the initial sentence
+# At the end, we count how much unique words are in the  list
+uniqueWords = [] 
+for word in list_sentence_split:
+      if word not in uniqueWords:
+          uniqueWords.append(word)
+
+uniqueWordsNumber = len(uniqueWords)
+print(f"There are {uniqueWordsNumber} unique word(s)")
